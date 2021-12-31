a516eff419de300e0f6ca7656b9742d457c2f9a5
alejandroBarraza <alejandro.barraza@alumnos.ucn.cl>
2021-12-31 01:57:08 -0300

    auth controller logic

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,9 +1,25 @@
 const { query } = require('../db');
+const hashedPassword = require('../utils/utils');
 //initial path routes: '/api/auth'
 
 //register user controller
 const register = async (req, res, next) => {
-    res.send('Register route testing');
+    // add validation for username,email and password;
+    const { username, email, password } = req.body;
+    const passwordHashed = await hashedPassword(password);
+    try {
+        const { rows } = await query(
+            `INSERT INTO users (username,email,password) VALUES ($1,$2,$3) RETURNING *`,
+            [username, email, passwordHashed]
+        );
+        res.status(200).json({
+            status: 'success',
+            results: rows.length,
+            user: rows,
+        });
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 // login user controller
