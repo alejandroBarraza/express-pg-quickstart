bdecdd68a1c561d107e1c4e6258dbc3ceff7d457
alejandroBarraza <alejandro.barraza@alumnos.ucn.cl>
2022-01-18 17:08:35 -0300

    private routes

--- src/controllers/privateRoutes.js
+++ src/controllers/privateRoutes.js
@@ -1,6 +1,7 @@
 const { query } = require('../db');
 
 const getAllUser = async (req, res) => {
+    console.log(req.user);
     try {
         const { rows } = await query('SELECT * FROM users');
         res.status(200).json({ success: true, rows });
