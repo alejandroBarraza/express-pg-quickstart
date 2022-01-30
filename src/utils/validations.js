179494c63d636b77854d97b687f061dd24a45e2f
alejandroBarraza <alejandro.barraza@alumnos.ucn.cl>
2022-01-30 17:23:05 -0300

    remove regex pass

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,27 +1,31 @@
-const Joi = require('joi');
+const Joi = require('joi')
 
 // Register validation schema
 const RegisterValidation = (data) => {
     const schema = Joi.object({
         username: Joi.string().alphanum().min(3).max(20).required(),
-        password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+        password: Joi.string()
+            // .pattern(new RegExp('^(?=.*[A-Za-z])(?=.*d)[A-Za-zd]{6,}$'))
+            .required(),
         email: Joi.string()
             .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
             .required(),
-    });
+    })
 
-    return schema.validate(data);
-};
+    return schema.validate(data)
+}
 
 // Login validation schema
 const loginValidation = (data) => {
     const schema = Joi.object({
         username: Joi.string().alphanum().min(3).max(20).required(),
-        password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-    });
+        password: Joi.string()
+            // .pattern(new RegExp('^(?=.*[A-Za-z])(?=.*d)[A-Za-zd]{6,}$'))
+            .required(),
+    })
 
-    return schema.validate(data);
-};
+    return schema.validate(data)
+}
 
 // forgotPassword validarion schema
 const forgotPasswordValidation = (data) => {
@@ -29,9 +33,9 @@ const forgotPasswordValidation = (data) => {
         email: Joi.string()
             .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
             .required(),
-    });
+    })
 
-    return schema.validate(data);
-};
+    return schema.validate(data)
+}
 
-module.exports = { RegisterValidation, loginValidation, forgotPasswordValidation };
+module.exports = { RegisterValidation, loginValidation, forgotPasswordValidation }
