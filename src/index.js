d5c8c431e6be84756bc0f4df3bddfe5e3f625fe3
alejandroBarraza <alejandro.barraza@alumnos.ucn.cl>
2022-11-15 22:33:14 +0900

    port

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 // imports
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const errorHandler = require('./middlewares/error');
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const errorHandler = require('./middlewares/error')
 
 // Express init
-const app = express();
-const PORT = process.env.PORT || APP_PORT;
+const app = express()
+const PORT = process.env.PORT || 4000
 
 // Middlewares
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //User routes middleware
 // app.use('/api/user', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/privated', require('./routes/privateRoutes'));
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/privated', require('./routes/privateRoutes'))
 
 //Error handler middleware
-app.use(errorHandler);
+app.use(errorHandler)
 
 //Launch Server
-console.log('REST API Backend Service Launching...');
-console.log('----------------------------------------------');
+console.log('REST API Backend Service Launching...')
+console.log('----------------------------------------------')
 
 //Express Startup
 app.listen(PORT, () => {
-    console.log(`[OK] Express listening on port ${PORT}`);
-});
+    console.log(`[OK] Express listening on port ${PORT}`)
+})
