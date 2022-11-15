0127e4d4e9e7cf2b8bd21d6dbd80a2083bd826d8
alejandroBarraza <alejandro.barraza@alumnos.ucn.cl>
2022-11-15 21:40:33 +0900

    prod config

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,16 @@
-const { Pool } = require('pg');
+const { Pool } = require('pg')
 
-const pool = new Pool();
+const prodConfig = process.env.DATABASE_URL
+const devConfig = {
+    user: password.env.PG_USER,
+    password: password.env.PG_PASSWORD,
+    host: password.env.PG_HOST,
+    database: password.env.PG_DATABASE,
+    port: password.env.PG_PORT,
+}
+
+const pool = new Pool(process.env.NODE_ENV === 'production' ? prodConfig : devConfig)
 
 module.exports = {
     query: (text, params) => pool.query(text, params),
-};
+}
